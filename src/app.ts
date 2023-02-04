a6fec05b3302b65ac1f037db3c5d3cd4c829966b
Snehal Dhameliya <snehaldhameliya17@gmail.com>
2023-02-04 18:59:05 +0530

    - Setup Environment Variables. - Setup Database Connection.

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,14 @@
 import express from "express";
+import * as dotenv from "dotenv";
+import { env } from "process";
+import { MongoClient } from "mongodb";
+
+dotenv.config();
 
 const app = express();
 
-app.listen(3000, "", () => {
-  console.log("Server Success 2");
-});
+MongoClient.connect(env.MONGO_URI!)
+  .then((d) => {
+    app.listen(+env.PORT!);
+  })
+  .catch((err) => console.log(err));
